89e5c69d647b393597a3bea8e0b8f2131c1f406e
Sergey Litvinov <slitvinov@gmail.com>
2024-06-11 16:46:47 +0200

    rm FCD

--- utils.py
+++ utils.py
@@ -13,34 +13,26 @@ class View(torch.nn.Module):
         return x.view(-1, *self.shape)
 
 
-class CDEFunc(torch.nn.Module):
+class NeuralCDE(torch.nn.Module):
 
-    def __init__(self, input_channels, hidden_channels):
+    def __init__(self, input_channels, hidden_channels, output_channels):
         super().__init__()
-        self.model = torch.nn.Sequential(
+        self.func = torch.nn.Sequential(
             torch.nn.Linear(hidden_channels, HIDDEN_LAYER_WIDTH),
             torch.nn.ReLU(),
             torch.nn.Linear(HIDDEN_LAYER_WIDTH,
                             input_channels * hidden_channels), torch.nn.Tanh(),
             View(hidden_channels, input_channels))
-
-    def forward(self, t, z):
-        return self.model(z)
-
-
-class NeuralCDE(torch.nn.Module):
-
-    def __init__(self, input_channels, hidden_channels, output_channels):
-        super().__init__()
-        self.func = CDEFunc(input_channels, hidden_channels)
-        CDEFunc(input_channels, hidden_channels)
         self.initial = torch.nn.Linear(input_channels, hidden_channels)
         self.readout = torch.nn.Linear(hidden_channels, output_channels)
 
     def forward(self, X):
         X0 = X.evaluate(X.interval[0])
         z0 = self.initial(X0)
-        z_T = torchcde.cdeint(X=X, z0=z0, func=self.func, t=X.interval)
+        z_T = torchcde.cdeint(X=X,
+                              z0=z0,
+                              func=lambda t, z: self.func(z),
+                              t=X.interval)
         z_T = z_T[:, 1]
         pred_y = self.readout(z_T)
         return pred_y
@@ -48,7 +40,6 @@ class NeuralCDE(torch.nn.Module):
 
 def get_data(num_timepoints):
     t = torch.linspace(0., 4 * math.pi, num_timepoints)
-
     start = torch.rand(HIDDEN_LAYER_WIDTH) * 2 * math.pi
     x_pos = torch.cos(start.unsqueeze(1) + t.unsqueeze(0)) / (1 + 0.5 * t)
     x_pos[:HIDDEN_LAYER_WIDTH // 2] *= -1
