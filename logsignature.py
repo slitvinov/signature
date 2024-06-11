1feb2ea30a2fa481ddcd496e65bda3f753efc491
Sergey Litvinov <slitvinov@gmail.com>
2024-06-11 12:37:10 +0200

    add utils.py

--- logsignature.py
+++ logsignature.py
@@ -2,80 +2,11 @@ import math
 import time
 import torch
 import torchcde
+import utils
 
-HIDDEN_LAYER_WIDTH = 64
+utils.HIDDEN_LAYER_WIDTH = 64
 NUM_EPOCHS = 10
 NUM_TIMEPOINTS = 5000
-
-
-class CDEFunc(torch.nn.Module):
-
-    def __init__(self, input_channels, hidden_channels):
-        super(CDEFunc, self).__init__()
-        self.input_channels = input_channels
-        self.hidden_channels = hidden_channels
-
-        self.linear1 = torch.nn.Linear(hidden_channels, HIDDEN_LAYER_WIDTH)
-        self.linear2 = torch.nn.Linear(HIDDEN_LAYER_WIDTH,
-                                       input_channels * hidden_channels)
-
-    def forward(self, t, z):
-        z = self.linear1(z)
-        z = z.relu()
-        z = self.linear2(z)
-        z = z.tanh()
-        z = z.view(z.size(0), self.hidden_channels, self.input_channels)
-        return z
-
-
-class NeuralCDE(torch.nn.Module):
-
-    def __init__(self,
-                 input_channels,
-                 hidden_channels,
-                 output_channels,
-                 interpolation="cubic"):
-        super(NeuralCDE, self).__init__()
-
-        self.func = CDEFunc(input_channels, hidden_channels)
-        self.initial = torch.nn.Linear(input_channels, hidden_channels)
-        self.readout = torch.nn.Linear(hidden_channels, output_channels)
-        self.interpolation = interpolation
-
-    def forward(self, coeffs):
-        if self.interpolation == 'cubic':
-            X = torchcde.NaturalCubicSpline(coeffs)
-        elif self.interpolation == 'linear':
-            X = torchcde.LinearInterpolation(coeffs)
-        else:
-            raise ValueError(
-                "Only 'linear' and 'cubic' interpolation methods are implemented."
-            )
-        X0 = X.evaluate(X.interval[0])
-        z0 = self.initial(X0)
-        z_T = torchcde.cdeint(X=X, z0=z0, func=self.func, t=X.interval)
-        z_T = z_T[:, 1]
-        pred_y = self.readout(z_T)
-        return pred_y
-
-
-def get_data(num_timepoints=100):
-    t = torch.linspace(0., 4 * math.pi, num_timepoints)
-
-    start = torch.rand(HIDDEN_LAYER_WIDTH) * 2 * math.pi
-    x_pos = torch.cos(start.unsqueeze(1) + t.unsqueeze(0)) / (1 + 0.5 * t)
-    x_pos[:HIDDEN_LAYER_WIDTH // 2] *= -1
-    y_pos = torch.sin(start.unsqueeze(1) + t.unsqueeze(0)) / (1 + 0.5 * t)
-    x_pos += 0.01 * torch.randn_like(x_pos)
-    y_pos += 0.01 * torch.randn_like(y_pos)
-    X = torch.stack(
-        [t.unsqueeze(0).repeat(HIDDEN_LAYER_WIDTH, 1), x_pos, y_pos], dim=2)
-    y = torch.zeros(HIDDEN_LAYER_WIDTH)
-    y[:HIDDEN_LAYER_WIDTH // 2] = 1
-    perm = torch.randperm(HIDDEN_LAYER_WIDTH)
-    X = X[perm]
-    y = y[perm]
-    return X, y
 
 
 def train_and_evaluate(train_X, train_y, test_X, test_y, depth, num_epochs,
@@ -85,10 +16,10 @@ def train_and_evaluate(train_X, train_y, test_X, test_y, depth, num_epochs,
                                            depth,
                                            window_length=window_length)
     print("Logsignature shape: {}".format(train_logsig.size()))
-    model = NeuralCDE(input_channels=train_logsig.size(-1),
-                      hidden_channels=8,
-                      output_channels=1,
-                      interpolation="linear")
+    model = utils.NeuralCDE(input_channels=train_logsig.size(-1),
+                            hidden_channels=8,
+                            output_channels=1,
+                            interpolation="linear")
     optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
     train_coeffs = torchcde.linear_interpolation_coeffs(train_logsig)
     train_dataset = torch.utils.data.TensorDataset(train_coeffs, train_y)
@@ -117,8 +48,8 @@ def train_and_evaluate(train_X, train_y, test_X, test_y, depth, num_epochs,
     return proportion_correct, elapsed
 
 
-train_X, train_y = get_data(num_timepoints=NUM_TIMEPOINTS)
-test_X, test_y = get_data(num_timepoints=NUM_TIMEPOINTS)
+train_X, train_y = utils.get_data(num_timepoints=NUM_TIMEPOINTS)
+test_X, test_y = utils.get_data(num_timepoints=NUM_TIMEPOINTS)
 depths = [1, 2, 3]
 window_length = 50
 accuracies = []
--- time_series_classification.py
+++ time_series_classification.py
@@ -1,84 +1,15 @@
-import sys
 import math
 import numpy as np
 import torch
 import torchcde
+import utils
 import matplotlib.pyplot as plt
 
-HIDDEN_LAYER_WIDTH = 64
+utils.HIDDEN_LAYER_WIDTH = 64
 NUM_EPOCHS = 10
 BATCH_SIZE = 32
 
-
-class CDEFunc(torch.nn.Module):
-
-    def __init__(self, input_channels, hidden_channels):
-        super(CDEFunc, self).__init__()
-        self.input_channels = input_channels
-        self.hidden_channels = hidden_channels
-
-        self.linear1 = torch.nn.Linear(hidden_channels, HIDDEN_LAYER_WIDTH)
-        self.linear2 = torch.nn.Linear(HIDDEN_LAYER_WIDTH,
-                                       input_channels * hidden_channels)
-
-    def forward(self, t, z):
-        z = self.linear1(z)
-        z = z.relu()
-        z = self.linear2(z)
-        z = z.tanh()
-        z = z.view(z.size(0), self.hidden_channels, self.input_channels)
-        return z
-
-
-class NeuralCDE(torch.nn.Module):
-
-    def __init__(self,
-                 input_channels,
-                 hidden_channels,
-                 output_channels,
-                 interpolation="cubic"):
-        super(NeuralCDE, self).__init__()
-        self.func = CDEFunc(input_channels, hidden_channels)
-        self.initial = torch.nn.Linear(input_channels, hidden_channels)
-        self.readout = torch.nn.Linear(hidden_channels, output_channels)
-        self.interpolation = interpolation
-
-    def forward(self, coeffs):
-        if self.interpolation == 'cubic':
-            X = torchcde.NaturalCubicSpline(coeffs)
-        elif self.interpolation == 'linear':
-            X = torchcde.LinearInterpolation(coeffs)
-        else:
-            raise ValueError(
-                "Only 'linear' and 'cubic' interpolation methods are implemented."
-            )
-        X0 = X.evaluate(X.interval[0])
-        z0 = self.initial(X0)
-        z_T = torchcde.cdeint(X=X, z0=z0, func=self.func, t=X.interval)
-        z_T = z_T[:, 1]
-        pred_y = self.readout(z_T)
-        return pred_y
-
-
-def get_data(num_timepoints=100):
-    t = torch.linspace(0., 4 * math.pi, num_timepoints)
-    start = torch.rand(HIDDEN_LAYER_WIDTH) * 2 * math.pi
-    x_pos = torch.cos(start.unsqueeze(1) + t.unsqueeze(0)) / (1 + 0.5 * t)
-    x_pos[:HIDDEN_LAYER_WIDTH // 2] *= -1
-    y_pos = torch.sin(start.unsqueeze(1) + t.unsqueeze(0)) / (1 + 0.5 * t)
-    x_pos += 0.01 * torch.randn_like(x_pos)
-    y_pos += 0.01 * torch.randn_like(y_pos)
-    X = torch.stack(
-        [t.unsqueeze(0).repeat(HIDDEN_LAYER_WIDTH, 1), x_pos, y_pos], dim=2)
-    y = torch.zeros(HIDDEN_LAYER_WIDTH)
-    y[:HIDDEN_LAYER_WIDTH // 2] = 1
-    perm = torch.randperm(HIDDEN_LAYER_WIDTH)
-    X = X[perm]
-    y = y[perm]
-    return X, y
-
-
-train_X, train_y = get_data()
+train_X, train_y = utils.get_data()
 fig, (ax1, ax2) = plt.subplots(1, 2, tight_layout=True)
 idx, = np.where(train_y == 0)
 ax1.plot(train_X[idx[0], :, 1], train_X[idx[0], :, 2])
@@ -93,7 +24,7 @@ ax2.set_xlabel("x")
 ax2.set_ylabel("y")
 plt.show()
 
-model = NeuralCDE(input_channels=3, hidden_channels=8, output_channels=1)
+model = utils.NeuralCDE(input_channels=3, hidden_channels=8, output_channels=1)
 optimizer = torch.optim.Adam(model.parameters())
 train_coeffs = torchcde.natural_cubic_coeffs(train_X)
 train_dataset = torch.utils.data.TensorDataset(train_coeffs, train_y)
@@ -110,7 +41,7 @@ for epoch in range(NUM_EPOCHS):
         optimizer.zero_grad()
     print('Epoch: {:2d}   Training loss: {}'.format(epoch, loss.item()))
 
-test_X, test_y = get_data()
+test_X, test_y = utils.get_data()
 test_coeffs = torchcde.natural_cubic_coeffs(test_X)
 pred_y = model(test_coeffs).squeeze(-1)
 binary_prediction = (torch.sigmoid(pred_y) > 0.5).to(test_y.dtype)
